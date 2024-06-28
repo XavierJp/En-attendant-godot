68e68992f5908d326e5f4ffacc15a6becf7fb74c
Douglas DUTEIL <douglasduteil@gmail.com>
2024-06-28 11:14:29 +0200

    feat: big ship

---
+++ scenes/enemies/SmallShip.gd
@@ -0,0 +1,13 @@
+extends Enemy
+
+func _ready():
+	const speed = 100000
+	var health = 500
+	player = get_node("/root/Level/Player")
+
+func move(delta: float):
+	var direction = global_position.direction_to(player.global_position)
+	var distance = global_position.distance_to(player.global_position)
+	velocity = direction * speed * (3 if distance > 500 else 1) * delta
+	rotation = direction.angle()
+	move_and_slide()

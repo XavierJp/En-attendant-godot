c6e48e0bcf8a07a6a6356dfa9ade76d2448c5176
xavier jouppe <xajouppe@gmail.com>
2024-06-28 23:09:00 +0200

    fix: game over & restart

--- scenes/UIs/game_over.gd
+++ scenes/UIs/game_over.gd
@@ -1,8 +1,7 @@
 extends Control
 
 func _on_button_pressed():
-	get_tree().change_scene_to_file("res://scenes/UIs/title_screen.tscn")
-
+	get_tree().change_scene_to_file("res://scenes/level/level.tscn")
 
 func _on_quit_button_pressed():
 	get_tree().quit()

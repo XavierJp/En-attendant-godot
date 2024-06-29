fb704db57b6e7c39107404c84768b19cba7f3922
felhix <felix.gaude@gmail.com>
2024-06-30 01:03:26 +0200

    with xavier advices

--- scenes/Weapons/Projectile/Gun/gun.gd
+++ scenes/Weapons/Projectile/Gun/gun.gd
@@ -1,28 +1,26 @@
 class_name Gun extends EntityProjectileWeapon
 
-var angles = [0]
 var damage = 100
+var level = 1
 
+func angles():
+	if level == 1:
+		return [0]
+	elif level == 2:
+		return [-PI/4,0,PI/4]
+	elif level == 3:
+		return [-PI/4,-PI/16,PI/16,PI/4]
+	elif level == 4:
+		return [-100, -50, 0, 50, 100]
 
 func _on_bullet_timer_timeout():
 	var bullet = preload("res://scenes/Weapons/Projectile/bullet.tscn")
-	for angle in angles:
+	for angle in angles():
 		var new_bullet = bullet.instantiate()
 		new_bullet.angle = angle
 		new_bullet.damage = damage
 		$ProjectileStartPosition.add_child(new_bullet)
 
 func upgrade():
-	damage = damage * 2
-	upgrade_angles()
+	level+=1
 
-func upgrade_angles():
-	var level = len(angles)
-	if level == 1:
-		angles = [-30,30]
-	elif level == 2:
-		angles = [-50,0,50]
-	elif level == 3:
-		angles = [-30,30]
-	elif level == 4:
-		angles = [-100, -50, 0, 50, 100]
--- scenes/Weapons/Zone/shield_weapon.gd
+++ scenes/Weapons/Zone/shield_weapon.gd
@@ -2,10 +2,12 @@ class_name Shield extends Area2D
 
 var damage = 25
 
+var level = 1
+
 func _process(_delta):
 	for body in self.get_overlapping_bodies():
 		if body.has_method("take_damage"):
-			body.take_damage(damage)
+			body.take_damage(damage*level)
 
 func upgrade():
-	damage = damage * 2
+	level+=1
